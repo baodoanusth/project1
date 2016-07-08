e3fdaede505f63f084e05f3095dbc5fcf3019644
baodoanusth <baodoan1131996@gmail.com>
2016-07-08 10:43:25 +0700

    User login

--- project1/config/routes.rb
+++ project1/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get '/home' => 'static_pages#home'
   get '/help' => 'static_pages#help'
-
+  get '/signup' => 'users#new'
   root 'static_pages#home'
+  post '/signup',  to: 'users#create'
+  resources :users
+
 end
