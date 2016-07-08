a2de3b1d83de53905456a19c06de38dc75ed8283
baodoanusth <baodoan1131996@gmail.com>
2016-07-08 11:02:49 +0700

    login logout

--- project1/config/routes.rb
+++ project1/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get '/home' => 'static_pages#home'
   get '/help' => 'static_pages#help'
   get '/signup' => 'users#new'
   root 'static_pages#home'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :users
 
 end
